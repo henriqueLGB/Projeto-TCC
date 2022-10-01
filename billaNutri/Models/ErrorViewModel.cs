9e3cf76263c8b15527c928f2130eda308ee68f82
henriqueLGB <henriquelirabilla@outlook.com.br>
2022-10-01 20:18:13 -0300

    Adicionar arquivos de projeto.

---
+++ billaNutri/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace billaNutri.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
